a0de3d67e03fb763f897e3eceac9e21b5790b378
David Peak <dppeak@yahoo.com>
2026-03-22 23:34:03 -0500

    chore: replace stub index.js with build redirect comment

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-'use strict'
-var Accessory, Service, Characteristic, UUIDGen
-
-module.exports = function (homebridge) {
-  Accessory = homebridge.platformAccessory
-  Service = homebridge.hap.Service
-  Characteristic = homebridge.hap.Characteristic
-  UUIDGen = homebridge.hap.uuid
-  homebridge.registerPlatform("homebridge-sleepiq", "SleepIQ", SleepIQPlatform, true)
-}
-
-class SleepIQPlatform {
-
-}
+// This file is intentionally left as a stub.
+// The plugin entry point is dist/index.js (compiled from src/index.ts).
+// Run `npm run build` to generate the dist/ folder before using this plugin.
